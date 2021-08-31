743717860ed23983e794a2cc518793fdbeb3070b
YvetteFB <61788237+YvetteFB@users.noreply.github.com>
2021-08-31 18:38:26 +0300

    overlay

--- app/js/script.js
+++ app/js/script.js
@@ -1,17 +1,27 @@
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
+const body = document.querySelector('body');
 
 
 btnHamburger.addEventListener('click', function(){
     if(header.classList.contains('open')){
+        body.classList.remove('noscroll');
         header.classList.remove('open');
-        header.classList.remove('fade-in');
-        overlay.classList.add('fade-out');
+
+        fadeElems.forEach(function(element){
+        element.classList.remove('fade-in');
+        element.classList.add('fade-out');
+        });
     }
     else {//open hamburger menu
+        body.classList.add('noscroll');
         header.classList.add('open');
-        overlay.classList.add('fade-in');
-        overlay.classList.remove('fade-out');
+        fadeElems.forEach(function(element){
+            element.classList.add('fade-in');
+            element.classList.remove('fade-out');
+        })
+ 
     }
 });
